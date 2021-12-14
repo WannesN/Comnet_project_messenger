42c70271a778705b8ae8dee2d02fa3f65ac0881a
WannesN <49029691+WannesN@users.noreply.github.com>
2021-12-14 14:07:29 +0100

    initial Winsock test

--- MessengingApp/MessengingApp/MessengingApp.cpp
+++ MessengingApp/MessengingApp/MessengingApp.cpp
@@ -1,7 +1,28 @@
 #include "MessengingApp.h"
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
 
 MessengingApp::MessengingApp(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+	WSADATA wsaData;
+
+	int iResult;
+	QString iResult_str;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	iResult_str = QString::number(iResult);
+	ui.DebugLabel->setText(iResult_str);
 }
